a04050cb9d16ed6c13b8431ca356125fb4e1f2ff
JIC <qlshdh@gmail.com>
2022-04-14 21:39:17 +0900

    add Post

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -1,14 +1,19 @@
 import React from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
   return (
-    <div>
+    <div className="post">
       <h3>UserName</h3>
       <img
+        className="post__image"
         src="http://newsimg.hankookilbo.com/2019/04/29/201904291390027161_3.jpg"
         alt=""
       />
-      <h4>username: caption</h4>
+      <h4 className="post__text">
+        <strong>Ditei</strong>caption
+      </h4>
     </div>
   );
 }
